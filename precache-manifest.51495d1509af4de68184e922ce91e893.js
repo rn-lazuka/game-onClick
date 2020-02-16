b8dd60020a175c7957a7a4aa2145deaadd3c944f
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-16 18:26:00 +0300

    Updates

---
+++ precache-manifest.51495d1509af4de68184e922ce91e893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba25ddb7596796a1edfb973444ee28d",
+    "url": "/game-onClick/index.html"
+  },
+  {
+    "revision": "e186a3e2013c22938e89",
+    "url": "/game-onClick/static/css/main.3fafe0ae.chunk.css"
+  },
+  {
+    "revision": "5de2c83b00939d7dbce9",
+    "url": "/game-onClick/static/js/2.6433289a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-onClick/static/js/2.6433289a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e186a3e2013c22938e89",
+    "url": "/game-onClick/static/js/main.372245ab.chunk.js"
+  },
+  {
+    "revision": "3a82c51e42660a5d55f7",
+    "url": "/game-onClick/static/js/runtime-main.5a42eaba.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/game-onClick/static/media/dog.75fa68d4.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/game-onClick/precache-manifest.51495d1509af4de68184e922ce91e893.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-onClick/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
