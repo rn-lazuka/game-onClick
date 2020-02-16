6f9d6edd9e0058bf954c69c8bd75bf797a7070bd
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-16 18:18:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Counter from "./Counter";
+
+
+class App extends React.Component {
+    state =
+        
+    render() {
+
+        return (
+            <div>
+                <Header/>
+                <Counter/>
+            </div>
+
+        );
+    }
+};
+
+export default App;
